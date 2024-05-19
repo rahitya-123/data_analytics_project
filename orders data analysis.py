d37c1f1ce1b09c5f8b72a378618b5bd38cbfe85b
Rahitya Velpuri <33275106+rahitya-123@users.noreply.github.com>
2024-05-19 12:30:12 +0530

    Update orders data analysis.py

--- orders data analysis.py
+++ orders data analysis.py
@@ -3,21 +3,16 @@
 
 # In[4]:
 
-
 #import libraries
 #!pip install kaggle
 import kaggle
 
-
 # In[ ]:
-
 
 #download a dataset using kaggle api
 get_ipython().system('kaggle datasets download ankitbansal06/retail-orders -f orders.csv')
 
-
 # In[6]:
-
 
 #extract file from zip file
 import zipfile
@@ -28,16 +23,13 @@ zip_ref.close() #close file
 
 # In[10]:
 
-
 #read data from the file and handle null values
 import pandas as pd
 df=pd.read_csv('orders.csv',na_values=['Not Available','unknown'])
 #df.head(20)
 df['Ship Mode'].unique()
 
-
 # In[33]:
-
 
 #rename column names ,make them lowercase and replace space with underscore
 #df
@@ -46,9 +38,7 @@ df['Ship Mode'].unique()
 #df.columns=df.columns.str.replace(' ','_')
 #df.head(5)
 
-
 # In[25]:
-
 
 #derive new columns discount,sale price and profit
 #df['discount']=df['list_price']*df['discount_percent']*0.01
@@ -56,51 +46,24 @@ df['Ship Mode'].unique()
 df['profit']=df['sale_price']-df['cost_price']
 df.head(5)
 
-
 # In[29]:
-
 
 #convert order date from object datatype to datetime
 df['order_date']=pd.to_datetime(df['order_date'],format='%Y-%m-%d')
 
-
 # In[32]:
-
 
 #drop cost price , list price,discount columns
 df.drop(columns=['list_price','cost_price','discount_percent'],inplace=True)
 
-
 # In[35]:
-
 
 #load the data into sql server using replace option
 import sqlalchemy as sal
 engine=sal.create_engine('mssql://RahityaGovindu/master?driver=ODBC+DRIVER+17+FOR+SQL+SERVER')
 conn=engine.connect()
 
-
 # In[37]:
-
 
 #load data into sql using append option
 df.to_sql('df_orders',con=conn,index=False,if_exists='append')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
